9cefc51a2ac8d0a7344a554af06edb7e91ad1a62
unknown <247websolutionsltd@gmail.com>
2026-08-18 13:34:07 +0100

    new update

--- src/models/Listing.js
+++ src/models/Listing.js
@@ -26,8 +26,7 @@ const listingSchema = new mongoose.Schema(
     },
 
     category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
+      type: String,
       required: true,
     },
 
