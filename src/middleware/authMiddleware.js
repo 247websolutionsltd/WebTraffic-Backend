31ef51474720c6d80039f43f6c10829f34769036
unknown <247websolutionsltd@gmail.com>
2026-08-28 11:18:23 +0100

    new update

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -17,7 +17,7 @@ const protect = (req, res, next) => {
 
     const decoded = jwt.verify(
       token,
-      process.env.JWT_SECRET
+      "webtraffic_super_secret_8f4c9e7b2a1d6f5c9e8a7b3d1f6e2c9a4b7d8e1f5c3a9b6d2e7f4"
     );
 
     req.user = decoded;
