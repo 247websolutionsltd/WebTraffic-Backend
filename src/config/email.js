859069f9dd427240a9fc893a13d572eb7a932ba9
unknown <247websolutionsltd@gmail.com>
2026-08-31 14:03:52 +0100

    new update

--- src/config/email.js
+++ src/config/email.js
@@ -1,12 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("EMAIL CONNECTION ERROR:", error);
+  } else {
+    console.log("EMAIL SERVER IS READY");
+  }
 });
 
 module.exports = transporter;
