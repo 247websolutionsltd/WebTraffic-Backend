1c4687f021acc1aafcfa9a735404794bc3cf85ce
unknown <247websolutionsltd@gmail.com>
2026-09-01 09:07:44 +0100

    new update

--- src/config/email.js
+++ src/config/email.js
@@ -3,6 +3,6 @@ const { Resend } = require("resend");
 const resend = new Resend(
   process.env.RESEND_API_KEY
 );
-
+resend.domains.create({ name: '247websolutions.com.ng' });
 module.exports = resend;
 
